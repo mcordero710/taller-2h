fa27b41acb63a268067787d8a57e48d8bb763cc7
Marvin <marvin.cordero@edify.cr>
2025-07-26 08:45:23 -0600

    add home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,28 @@
-// src/Home.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaUsers, FaTruck, FaBoxes, FaTools } from 'react-icons/fa';
+import logo from '../assets/logo.png';
+import './Home.css';
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <h1>Bienvenido a la Página de Inicio</h1>
-      <nav>
+    <div className="home-layout">
+      <aside className="home-sidebar">
+        <div className="home-logo-container">
+          <img src={logo} alt="Taller 2H Logo" className="home-logo" />
+          <h1 className="home-title">Taller 2H</h1>
+        </div>
         <ul>
-          <li>
-            <Link to="/admin">Panel de Administración</Link>
-          </li>
-          <li>
-            <Link to="/profile">Mi Perfil</Link> {/* Agregar más enlaces si lo necesitas */}
-          </li>
-          <li>
-            <Link to="/settings">Configuraciones</Link>
-          </li>
+          <li><Link to="/clientes"><FaUsers className="menu-icon" /> Clientes</Link></li>
+          <li><Link to="/proveedores"><FaTruck className="menu-icon" /> Proveedores</Link></li>
+          <li><Link to="/inventario"><FaBoxes className="menu-icon" /> Inventario</Link></li>
+          <li><Link to="/ordenes"><FaTools className="menu-icon" /> Órdenes de trabajo</Link></li>
         </ul>
-      </nav>
+      </aside>
+
+      <main className="home-content">
+        <h2>Bienvenido a la Página de Inicio</h2>
+      </main>
     </div>
   );
 };
